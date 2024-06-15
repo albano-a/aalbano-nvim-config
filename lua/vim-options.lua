21429b938f5695489021bb629a8b698b92293f1a
André Luiz Albano <110417140+albano-a@users.noreply.github.com>
2024-06-15 01:38:17 -0300

    Added Harpoon, Fugitive

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,3 +1,5 @@
+-- set
+
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
@@ -7,6 +9,25 @@ vim.opt.relativenumber=true
 vim.g.mapleader = " "
 
 vim.opt.wrap = false
+
+vim.opt.smartindent = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
 
 -- REMAPS
 -- Save command
