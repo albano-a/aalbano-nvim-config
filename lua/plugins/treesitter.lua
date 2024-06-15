3c1624d232dc56cba6a5d6e8b72e41be39cdce00
André Luiz Albano <110417140+albano-a@users.noreply.github.com>
2024-06-15 10:22:49 -0300

    Fix: tree-sitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,9 +2,10 @@ return {
     "nvim-treesitter/nvim-treesitter", 
     build = ":TSUpdate",
     config = function()
-        require 'nvim-treesitter.install'.compilers = { "clang" }
+        -- require 'nvim-treesitter.install'.compilers = { "clang" }
         local config = require('nvim-treesitter.configs')
         config.setup({
+            ensure_installed = {"cmake", "vim", "vimdoc", "lua"},
             auto_install = true,
             highlight = { enable = true},
             indent = { enable = true },
