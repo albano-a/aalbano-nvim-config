b2c0ee5c1c070b614b7a72be095804fe5a2427c3
André Luiz Albano <110417140+albano-a@users.noreply.github.com>
2024-06-17 00:58:46 -0300

    Removed latex

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -3,7 +3,7 @@ return {
         "williamboman/mason.nvim",
         config = function()
             require("mason").setup()
-        end
+        end,
     },
     {
         "williamboman/mason-lspconfig.nvim",
@@ -17,19 +17,28 @@ return {
         lazy = false,
 
         config = function()
-            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
             local lspconfig = require("lspconfig")
             local servers = {
-                "lua_ls", "clangd", "cssls", "emmet_language_server", "emmet_ls", "rust_analyzer","tsserver", "html", "ltex", "marksman", "pyright"
+                "lua_ls",
+                "clangd",
+                "cssls",
+                "emmet_language_server",
+                "emmet_ls",
+                "rust_analyzer",
+                "tsserver",
+                "html",
+                "marksman",
+                "pyright",
             }
             for _, lsp in ipairs(servers) do
                 lspconfig[lsp].setup({
-                    capabilities = capabilities
+                    capabilities = capabilities,
                 })
-                vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-                vim.keymap.set({ 'n' }, '<leader>ca', vim.lsp.buf.code_action, {})
+                vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+                vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
             end
-        end
-    }
+        end,
+    },
 }
